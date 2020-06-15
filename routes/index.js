d6bad4cc6cb6a077a71a09f9c8514c434c4e8b82
Rajiv Chockalingam <rajivchockalingam@Rajivs-MBP.hsd1.tx.comcast.net>
2020-06-14 19:07:06 -0500

    Bootstap + Index View

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Shopping Cart" });
+router.get("/", (req, res, next) => {
+  res.render("shop/index", { title: "Online Shop" });
 });
 
 module.exports = router;
